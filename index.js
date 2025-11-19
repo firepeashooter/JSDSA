131e601a30171e560ab6fd9212831cd320ed847b
Ben Braun <benjamin.braun2003@gmail.com>
2025-11-18 23:51:34 -0500

    Finished Recursion Practice

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function productOfArray(arr){
 
 
 }
-
+//crazy shit but I think this is a depth first search
 function contains(obj, value){
 
     
@@ -96,4 +96,58 @@ let testObject = {
     }
 }
 
-console.log(contains(testObject, 14));
+
+
+//Basically a depth first search where we look for any numbers and add them to the total.
+
+function count(arr){
+
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i++){
+
+        if (Number.isInteger(arr[i])){
+            total += 1;
+        } else if (Array.isArray(arr[i])){
+            total += count(arr[i]);
+        }
+    }
+
+    return total;
+
+
+}
+
+
+function sumSquares(arr){
+
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i++){
+
+        if (Number.isInteger(arr[i])){
+            let square = arr[i] * arr[i];
+            total += square;
+        } else if (Array.isArray(arr[i])){
+            total += sumSquares(arr[i]);
+        }
+    }
+
+    return total;
+}
+
+
+function replicate(times, num){
+
+    if (times <= 0){
+        return [];
+    } else {
+        return [num].concat(replicate(times-1, num));
+        
+    }
+    
+}
+
+console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // []
