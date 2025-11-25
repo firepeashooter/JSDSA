e8e7a91dbd58de6956c97d3e603771374bb205f4
Ben Braun <benjamin.braun2003@gmail.com>
2025-11-25 12:09:04 -0500

    Add more functionality to linkedList class

--- linkedList.js
+++ linkedList.js
@@ -10,6 +10,7 @@ class LinkedList {
 	constructor() {
 		this.head = null;
 		this.tail = this.head;
+		this.size = 0;
 	}
 
 	append(value) {
@@ -22,6 +23,43 @@ class LinkedList {
 		} else {
 			this.tail.next = newNode;
 			this.tail = newNode;
+		}
+		this.size += 1;
+	}
+
+	prepend(value) {
+
+		let newNode = new Node(value);
+
+		newNode.next = this.head;
+		this.head = newNode;
+		this.size += 1;
+
+	}
+
+	sizes() {
+		return this.size;
+	}
+
+	heads() {
+		return this.head;
+	}
+
+	tails() {
+		return this.tail;
+	}
+
+	at(index) {
+
+		let n = this.head;
+		let curIndex = 0;
+
+		while (n) {
+			if (curIndex == index) {
+				return n;
+			}
+			n = n.next;
+			curIndex += 1;
 		}
 	}
 
@@ -44,6 +82,14 @@ const myLinkedList = new LinkedList();
 myLinkedList.append(3);
 myLinkedList.append(2);
 myLinkedList.append(6);
+
+myLinkedList.prepend(4);
+myLinkedList.prepend(1);
+
+console.log(`Head: ${myLinkedList.heads().value}`);
+console.log(`Tail: ${myLinkedList.tails().value}`);
+console.log(`Size: ${myLinkedList.sizes()}`);
+console.log(`Index of 3: ${myLinkedList.at(3).value}`);
 
 myLinkedList.printL();
 
