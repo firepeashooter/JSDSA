679268e91ed5136e8748df71e028d1216547578e
Ben Braun <benjamin.braun2003@gmail.com>
2025-11-26 13:35:11 -0500

    Add file for Tree Practice

---
+++ balancedBST.js
@@ -0,0 +1,22 @@
+
+class Node {
+
+	constructor(value, left, right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+
+	}
+}
+
+class Tree {
+
+	constructor(arr) {
+		this.root = buildTree(arr);
+
+	}
+
+	buildTree(arr) {
+
+	}
+}
