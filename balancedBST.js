5f7aba3b4c875a44da07fa4225aa8f0e953d4cf6
Ben Braun <benjamin.braun2003@gmail.com>
2025-12-10 18:52:46 -0500

    Adds BST build from array function

--- balancedBST.js
+++ balancedBST.js
@@ -1,7 +1,7 @@
 
-class Node {
+class TreeNode {
 
-	constructor(value, left, right) {
+	constructor(value, left = null, right = null) {
 		this.value = value;
 		this.left = left;
 		this.right = right;
@@ -9,14 +9,59 @@ class Node {
 	}
 }
 
-class Tree {
+class BinarySearchTree {
 
 	constructor(arr) {
-		this.root = buildTree(arr);
+
+		//Remove duplicates and sort the array for our BST algorithm
+		let noDupesArr = [...new Set(arr)];
+		let sortedArr = [...noDupesArr];
+		sortedArr.sort((a, b) => a - b);
+
+		this.root = this.buildBST(sortedArr, 0, sortedArr.length - 1);
 
 	}
 
-	buildTree(arr) {
+	buildBST(arr, start, end) {
 
+		if (start > end) {
+			return null;
+		}
+
+		let mid = Math.floor((start + end) / 2);
+
+		let root = new TreeNode(arr[mid]);
+
+		root.right = this.buildBST(arr, mid + 1, end);
+		root.left = this.buildBST(arr, start, mid - 1);
+
+		return root;
+	}
+
+	prettyPrint(node, prefix = '', isLeft = true) {
+		if (node === null) {
+			return;
+		}
+		if (node.right !== null) {
+			this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+		}
+		console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+		if (node.left !== null) {
+			this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+		}
 	}
 }
+
+
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let BST = new BinarySearchTree(arr);
+console.log(BST.root);
+BST.prettyPrint(BST.root);
+console.log(arr);
+
+
+
+
+
+
+
